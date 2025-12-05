49e96b1a3782e22e4d81ebdc4169f9ccc7bba0bd
Vinod Sirvi <vinodsirvi021@gmail.com>
2025-12-05 15:08:44 +0530

    Array Programs initiated

--- Array/src/com/array/program2.java
+++ Array/src/com/array/program2.java
@@ -26,13 +26,15 @@ public class program2 {
        
        int max = arr[0];
        
+    //   int max = Integer.MIN_VALUE;
+       
        for(int i=0 ; i<arr.length; i++) {
     	   
-//    	   if(arr[i] > max) {
-//    		   
-//    		   max = arr[i];
-    	   
-    	   max = Math.max(max, arr[i]);
+    	   if(arr[i] > max) {
+    		   
+    		   max = arr[i];
+       }
+ //   	   max = Math.max(max, arr[i]);
     	   
        }
        
--- Array/src/com/array/program3.java
+++ Array/src/com/array/program3.java
@@ -1,7 +1,29 @@
+// finding second largest and so on in an array
+
 package com.array;
 
 public class program3 {
    public static void main(String[] args) {
 	
+	   int arr[] = {1,34,21,6,4};
+	   
+	   int max = arr[0];
+	   int second = arr[0];
+	   for(int i =0; i<arr.length; i++) {
+		   
+		   if(arr[i] > max) {
+			 
+			   max = arr[i];
+		   } 
+		   
+		   else   if(arr[i] > second && second < max) {
+			  
+			   second = arr[i];
+		   }
+		   
+	   }
+	   
+	   
+	   System.out.println("largest:-"+max +" second:-"+second);
 }
 }
